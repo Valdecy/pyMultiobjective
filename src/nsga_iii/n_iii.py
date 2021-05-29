21a1d85b641fb588307cac3f6bd53330af54e34a
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-05-29 10:08:25 -0300

    Update n_iii.py

--- src/nsga_iii/n_iii.py
+++ src/nsga_iii/n_iii.py
@@ -94,14 +94,10 @@ def fast_non_dominated_sorting(population, number_of_functions = 2):
 
 # Function: Sort Population by Rank
 def sort_population_by_rank(population, rank):
-    idx            = np.argsort(rank, axis = 0)
-    rank_new       = np.zeros((population.shape[0], 1))
-    population_new = np.zeros((population.shape[0], population.shape[1]))  
-    for i in range(0, population.shape[0]):
-        rank_new[i, 0] = rank[idx[i], 0] 
-        for k in range(0, population.shape[1]):
-            population_new[i,k] = population[idx[i],k]
-    return population_new, rank_new
+    idx        = np.argsort(rank[:,0], axis = 0).tolist()
+    rank       = rank[idx,:]
+    population = population[idx,:]
+    return population, rank
 
 # Function: Offspring
 def breeding(population, min_values = [-5,-5], max_values = [5,5], mu = 1, list_of_functions = [func_1, func_2]):
