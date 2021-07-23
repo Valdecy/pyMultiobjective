8aca8326c896afea0801853195257f646ae3a8bb
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-07-23 17:15:25 -0300

    Update u_n_iii.py

--- src/u_nsga_iii/u_n_iii.py
+++ src/u_nsga_iii/u_n_iii.py
@@ -232,7 +232,6 @@ def association(population, srp, M):
     for ind in np.unique(arg).tolist():
         f = [i[0] for i in np.argwhere(arg == ind).tolist()]
         idx.append(f[d[f].argsort()[0]])
-    idx.extend([x for x in list(range(0, population.shape[0])) if x not in idx])
     return idx
 
 # Function: Sort Population by Association
