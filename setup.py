6370e99fc53aa1f7fb3f6c913441822c7a953c7c
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-05-27 18:09:43 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pyMultiobjective',
+    version='1.1.1',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyMultiobjective',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'plotly',
+        'pygmo',
+        'scipy'
+    ],
+    description='A python library for Multiobjective Objectives Optimization Algorithms or Many Objectives Optimization Algorithms',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
