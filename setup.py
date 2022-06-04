da4ecc55854c17501f1d16bac850afae8720fc10
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-06-04 18:14:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyMultiobjective',
-    version='1.2.1',
+    version='1.2.4',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
